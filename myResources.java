38a96a38afc3e75a3579186404087fcefc338a44
Joakes19 <30535015+Joakes19@users.noreply.github.com>
2018-03-04 16:46:18 +0000

    String list and resource bundle
    Added a string list and a basic resource bundle

---
+++ myResources.java
@@ -0,0 +1,31 @@
+// default (English language, United Kingdom)
+ public class MyResources extends ResourceBundle {
+     public Object handleGetObject(String key) {
+         if (key.equals("okKey")) return "Ok";
+         if (key.equals("cancelKey")) return "Cancel";
+         return null;
+     }
+
+     public Enumeration<String> getKeys() {
+         return Collections.enumeration(keySet());
+     }
+
+     // Overrides handleKeySet() so that the getKeys() implementation
+     // can rely on the keySet() value.
+     protected Set<String> handleKeySet() {
+         return new HashSet<String>(Arrays.asList("okKey", "cancelKey"));
+     }
+ }
+
+ // German language
+ public class MyResources_jp extends MyResources {
+     public Object handleGetObject(String key) {
+         // don't need okKey, since parent level handles it.
+         if (key.equals("cancelKey")) return "ƒLƒƒƒ“ƒZƒ‹";
+         return null;
+     }
+
+     protected Set<String> handleKeySet() {
+         return new HashSet<String>(Arrays.asList("cancelKey"));
+     }
+ }

51bb06724ba0503295af32a17a8d6ec4d5fc2239
Tyler Prescott <tyler.prescott@hotmail.co.uk>
2018-04-21 20:48:47 +0100

    Removal of reportTime
    Removal of report time as an item to be handed in on write report records as the Date object in java.util can handle both the date and time in one object, instead of having two separate objects

--- ReportWriter.java
+++ ReportWriter.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.*;
 
 public class ReportWriter extends DatabaseWriter{//Writes all information about reports in the system
-	void writeReportRecords(int clientID,int reportedClientID, int reportDate, int reportTime, int reportType, String reportReason,int reportEvidenceID){
+	void writeReportRecords(int clientID,int reportedClientID, Date reportDate, int reportType, String reportReason,int reportEvidenceID){
 		
 	}
 	

b23f5fe1e30250cc0bfe53b6ce4f7d7e10739f1f
Tyler Prescott <tyler.prescott@hotmail.co.uk>
2018-04-26 14:29:58 +0100

    Database file
    I have previously over complicated the implementation in an attempt to make using the database easier. This file should be all that's needed to use the database correctly.

--- Database.java
+++ Database.java
@@ -5,13 +5,14 @@ public class Database{//Base class that sets up the connection to the database
 	//Set up connection to database, set any constants needed for using the database
 	//Note - This database class is not meant to be initialised but creates the connection with the database that is needed to read and write tables
 	String databaseName, databaseUsername, databasePassword;
+	ResultSet resultSet;
 	public Database(String inputDatabaseName, String inputDatabaseUsername, String inputDatabasePassword){
 		databaseName = inputDatabaseName;
 		databaseUsername = inputDatabaseUsername;
 		databasePassword = inputDatabasePassword;
 	}
 	
-	private ResultSet executeQuery(String query){
+	public void executeQuery(String query){
 		try(
 			Connection connection = DriverManager.getConnection(
 				"jdbc:mysql://localhost:3306/" + databaseName + "?useSSL=false"
@@ -23,13 +24,23 @@ public class Database{//Base class that sets up the connection to the database
 			Statement statement = connection.createStatement();
 			)
 			{
-				ResultSet resultSet = statement.executeQuery(query);
-				return(resultSet);//This will change from statement to statement so handleQuery
+				ResultSet resultSet = statement.executeQuery(query);//This will change from statement to statement so handleQuery
 				//will change from class to class
 			}
+			
 		catch(SQLException ex) {
 			ex.printStackTrace();
 		}
 	}
 	//Possibly create a re connect function to re start the connection to the database if needed
+	
+	public ResultSet executeQueryWithResults(String statement){
+		executeQuery(statement);
+		return resultSet;
+	}
+	
+	public void executeQueryWithoutResults(String statement){
+		executeQuery(statement);//This executes the query on the database
+		//The result set is not returned as there would be no results from the query
+	}
 }
